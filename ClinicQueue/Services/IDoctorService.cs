103a429540ac9268eadeb3aa7d6c0eb59407552f
Kuldeep Thakur <kuldeepthakur48388@gmail.com>
2026-03-20 13:35:16 +0530

    chore: remove old monolith code - Clean Architecture only

--- ClinicQueue/Services/IDoctorService.cs
+++
@@ -1,8 +0,0 @@
-namespace ClinicQueue.Services
-{
-    public interface IDoctorService
-    {
-        Task<int?> GetDoctorStatusAsync(string doctorId);
-        Task SetDoctorStatusAsync(string doctorId, int status);
-    }
-}
